c7b187394707653774116a58c05239dc1749272a
yan.1307 <yan.1307@buckeyemail.osu.edu>
2025-04-18 16:08:45 -0700

    Set up plane movement with player input

---
+++ Scenes/Plane/Plane.gd
@@ -0,0 +1,37 @@
+extends CharacterBody2D
+
+const JUMP_POWER: float = -350.0
+# remember: _ means class level variable, so everything in this class/file can access this variable
+var _gravity = ProjectSettings.get("physics/2d/default_gravity");
+
+# reference so that we can use the animation player !!!! thats how we reference child nodes etc
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+	#pass
+	
+func _physics_process(delta: float) -> void:
+	jump(delta)
+	move_and_slide() # tells the gravity to take effect
+	
+	if is_on_floor():
+		die()
+
+func jump(delta: float):
+	velocity.y += _gravity * delta;
+	if(Input.is_action_just_pressed("jump")):
+		velocity.y = JUMP_POWER
+		animation_player.play("jump")
+		
+func die():
+		animated_sprite_2d.stop() # remember to right click and Open Documentation if ur curious on what the node can do
+		set_physics_process(false) #remember to set the process to false to prevent issues or more processing that we dont know about
+		# !! if this wasn't set to false, the physics_process function would still be running, AKA the player could still jump even if the animation is stopped.
