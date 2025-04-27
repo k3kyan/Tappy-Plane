63a97bf619ed8c08006b3742f0e5671d0da8ce05
yan.1307 <yan.1307@buckeyemail.osu.edu>
2025-04-27 02:57:55 -0400

    Implemented object persistence aka saved data that will persist even after game exits
    im pretty sure its persistance and not permanence..?
    To save data, use custom resources and @export to save data. And then set those variables somewhere.

--- Globals/ScoreManager.gd
+++ Globals/ScoreManager.gd
@@ -1,5 +1,7 @@
 extends Node
 
+const SCORES_PATH: String = "user://tappy.tres"
+
 var highscore: int = 0
 # properties ..... ??
 var highscore_gs: int:
@@ -8,7 +10,21 @@ var highscore_gs: int:
 	set(value):
 		if value > highscore:
 			highscore = value;
+			save_high_score()
 			
 # load up high scores when the game is booted
 func _ready() -> void:
-	pass
+	load_high_score()
+
+func save_high_score() -> void:
+	var hsr: HighScoreResource = HighScoreResource.new();
+	hsr.high_score = highscore;
+	#ResourceSaver.save(RESOURCE_I_WANT_TO_SAVE, SYSTEM_DIRECTORY_PATH_TO_SAVE_IT_TO)
+	ResourceSaver.save(hsr, SCORES_PATH)
+	
+func load_high_score() -> void:
+#	 check if we have a resource saved that exists first, so game doesnt crash loading something it doesnt have
+	if ResourceLoader.exists(SCORES_PATH):
+		var hsr: HighScoreResource = ResourceLoader.load(SCORES_PATH)
+		if hsr: # if hsr has a value in it
+			highscore = hsr.high_score
